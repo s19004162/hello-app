dd9477404424992f1f7e70a463a252ff7df7417e
TSUYOSHI KIMOTO <tk_automate@outlook.com>
2022-03-13 19:44:03 +0900

    Add sayho method

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def main():
 def hello():
     return 'I am good, how about you?'
 
+@app.route('/sayho')
+def sayho():
+    return 'Say, Ho, Ho, Ho!!'
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
